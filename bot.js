7c4706c81e40a2eb69e334cebdaea05ac3217f1e
Mikhail Lapshin <lapshin.michail@gmail.com>
2016-07-20 11:40:23 +0300

    уже почти играбельно =)

--- bot.js
+++ bot.js
@@ -13,6 +13,8 @@ var Utilites = {};
 var bot = new TelegramBot(token, botOptions);
 var aboutMe = null;
 
+var session = [];
+
 var
     CARDS_ARRAY = [
         '2',
@@ -29,6 +31,21 @@ var
         'K',
         'A',
     ],
+    CARDS_VALUES = {
+        '2' : 2,
+        '3' : 3,
+        '4' : 4,
+        '5' : 5,
+        '6' : 6,
+        '7' : 7,
+        '8' : 8,
+        '9' : 9,
+        '10' : 10,
+        'J' : 2,
+        'Q' : 3,
+        'K' : 4,
+        'A' : 11,
+    },
     COLORS_ARRAY = [
         '♥',
         '♦',
@@ -52,6 +69,9 @@ bot.on('text', function(msg)
     var messageUsr = msg.from.username;
     var firstName = msg.from.first_name;
 
+    if(!session[messageChatId])
+        session[messageChatId] = {};
+
     if (messageText === '/say') {
         sendMessageByBot(messageChatId, 'Hello World!');
         return;
@@ -63,12 +83,26 @@ bot.on('text', function(msg)
 
     if (messageText === '/start') {
         sendMessageByBot(messageChatId, 'Привет, ' + firstName + '! Давай сыграем в блекджек?');
-        sendMessageByBot(messageChatId, 'Вот твоя карта ' + getNewCard() );
+        sendMessageByBot(messageChatId, 'Напиши /run');
+        //sendMessageByBot(messageChatId, 'Вот твоя карта ' + getNewCard() );
         return;
     }
 
-    if (messageText === 'еще') {
-        sendMessageByBot(messageChatId, 'держи ' + getNewCard() );
+    if (messageText === '/run') {
+        sendMessageByBot(messageChatId, 'Вот твои карты ' + getNewCard(messageChatId, 'pl') + ' ' + getNewCard(messageChatId, 'pl'));
+        sendMessageByBot(messageChatId, 'Вот карты дилера' + getNewCard(messageChatId, 'dl') + ' ' + getNewCard(messageChatId, 'dl'));
+        sendMessageByBot(messageChatId, 'Если нужно еще, напиши /еще');
+        sendMessageByBot(messageChatId, 'Если хватит, напиши /хватит');
+        return;
+    }
+
+    if (messageText === '/еще') {
+        sendMessageByBot(messageChatId, 'держи ' + getNewCard(messageChatId, 'pl') );
+        return;
+    }
+
+    if (messageText === '/хватит') {
+        sendMessageByBot(messageChatId, 'Ты ' + checkCards(messageChatId));
         return;
     }
 
@@ -87,11 +121,32 @@ function sendMessageByBot(aChatId, aMessage)
     bot.sendMessage(aChatId, aMessage, { caption: 'I\'m a cute bot!' });
 }
 
-function getNewCard () {
+function getNewCard (chatId, who) {
     var rndValue = Utilites.randomInteger(0,12);
     var rndColor = Utilites.randomInteger(0,3);
+    if(chatId){
+        if(!session[chatId][who])
+            session[chatId][who] = [];
+        session[chatId][who].push(rndValue);
+    }
     return CARDS_ARRAY[rndValue] + COLORS_ARRAY[rndColor];
 };
+
+function checkCards(chatId) {
+    var dealerSum = 0, playerSum = 0;
+    session[chatId].dl.forEach(function(card){
+        dealerSum = dealerSum + CARDS_VALUES[card];
+    });
+    session[chatId].pl.forEach(function(card){
+        playerSum = playerSum + CARDS_VALUES[card];
+    });
+    if(playerSum > 21)
+        return 'проиграл, у тебя ' + playerSum + ' очков';
+    if(playerSum < 21 && playerSum > dealerSum)
+        return 'выиграл, у тебя ' + playerSum + ' очков';
+    else
+        return 'проиграл, у тебя ' + playerSum + ' очков';
+}
 
 
 Utilites.randomInteger = function(min, max) {
